fc6343f8d255478cdea8a2308a718c0dddebfb36
Matthew Baya <chiptiep99@gmail.com>
2024-04-29 20:34:56 +0800

    feat: mengupdate entitas utama (Update/PUT)

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -26,12 +26,62 @@ class ArticleController {
     }
   }
 
-  static async getAllArticle(req, res) {
+  static async getAllArticles(req, res) {
     try {
       let articles = await Article.findAll();
       res.status(200).json(articles);
     } catch (error) {
       res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
+  static async getArticleById(req, res) {
+    try {
+      let article = await Article.findByPk(req.params.id);
+      if (!article) {
+        throw { name: "InvalidData" };
+      }
+      res.status(200).json(article);
+    } catch (error) {
+      if (error.name === "InvalidData") {
+        res.status(404).json({ message: "Data not found" });
+      } else {
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    }
+  }
+
+  static async updateArticleById(req, res) {
+    try {
+      let { title, content, imgUrl, categoryId, authorId } = req.body;
+      let article = await Article.findByPk(req.params.id);
+      if (!article) {
+        throw { name: "InvalidData" };
+      }
+      await Article.update(
+        {
+          title,
+          content,
+          imgUrl,
+          categoryId,
+          authorId,
+        },
+        { where: { id: req.params.id } }
+      );
+      res.status(200).json({ message: "Data has been updated" });
+    } catch (error) {
+      console.log(error.name);
+      if (error.name === "SequelizeValidationError") {
+        res.status(400).json({
+          message: error.errors.map((e) => {
+            return e.message;
+          }),
+        });
+      } else if (error.name === "InvalidData") {
+        res.status(404).json({ message: "Data not found" });
+      } else {
+        res.status(500).json({ message: "Internal Server Error" });
+      }
     }
   }
 }
--- routes/article-route.js
+++ routes/article-route.js
@@ -2,6 +2,8 @@ const ArticleController = require("../controllers/ArticleController");
 const router = require("express").Router();
 
 router.post("/", ArticleController.postNewArticle);
-router.get("/", ArticleController.getAllArticle);
+router.get("/", ArticleController.getAllArticles);
+router.get("/:id", ArticleController.getArticleById);
+router.put("/:id", ArticleController.updateArticleById);
 
 module.exports = router;
